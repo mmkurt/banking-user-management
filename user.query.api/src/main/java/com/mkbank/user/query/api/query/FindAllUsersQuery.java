8d557ec6e0e2548308efa4ff558b314e355bb1f1
Mustafa Kurt <mustafa.kurt1@huawei.com>
2021-03-29 23:07:10 +0300

    Create user queries. closes #10.

---
+++ user.query.api/src/main/java/com/mkbank/user/query/api/query/FindAllUsersQuery.java
@@ -0,0 +1,5 @@
+package com.mkbank.user.query.api.query;
+
+
+public class FindAllUsersQuery {
+}
---
+++ user.query.api/src/main/java/com/mkbank/user/query/api/query/FindUserByIdQuery.java
@@ -0,0 +1,9 @@
+package com.mkbank.user.query.api.query;
+
+import lombok.Data;
+
+@Data
+public class FindUserByIdQuery {
+
+    private String id;
+}
