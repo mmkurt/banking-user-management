541dbf8ec1a735544eb783637f9a8e3763818bd1
Mustafa Kurt <mustafa.kurt1@huawei.com>
2021-04-03 23:49:27 +0300

    Add spring security. closes #13.

---
+++ user.oauth2/src/test/java/com/mkbank/user/oauth2/AuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkbank.user.oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
